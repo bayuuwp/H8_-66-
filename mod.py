2d2bfacc2eb35f7a4bac569804279d5d6029aefd
Bayu Wichaksono Pangestu <92321545+bayuuwp@users.noreply.github.com>
2021-10-25 18:54:02 +0700

    Add files via upload
    Initial Add Sesi 3

---
+++ mod.py
@@ -0,0 +1,13 @@
+s = "Hacktiv9-PTP Python For Data Science"
+a = [100,200,300]
+foo = (['quux', 'corge', 'grault])
+arg = ['quux', 'corge', 'graul]
+x = foo
+
+def foo(arg):
+    print(f'arg= {arg}')
+    
+
+
+class foo:
+    pass
